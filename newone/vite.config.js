5bbd0dfe50de635200870b904db3bcfb6e67ee11
deepakshet-tech <deepaknagarajshet@gmail.com>
2026-04-20 00:15:57 +0530

    something

--- newone/vite.config.js
+++ newone/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-content: ["./index.html", "./src/**/*.{js,jsx}"];
+// content: ["./index.html", "./src/**/*.{js,jsx}"];
 
 // https://vite.dev/config/
 export default defineConfig({
